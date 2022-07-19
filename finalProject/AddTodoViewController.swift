e41b5ec1ace0bdf0c12b89a1d2021a687f9a277d
Prasi Aravind <prasi040707@gmail.com>
2022-07-19 16:42:34 -0700

    Iteration 1- Todo List!

---
+++ finalProject/AddTodoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddTodoViewController.swift
+//  finalProject
+//
+//  Created by Prasi Aravind on 7/19/22.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    var previousVC = TodoTableViewController()
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+
+          if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+          }
+          previousVC.toDos.append(toDo)
+          previousVC.tableView.reloadData()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }*/
+    
+}
